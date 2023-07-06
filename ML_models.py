Fixing PCA and data leakage

ML_models.py
@@ -13,15 +13,18 @@ from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 
 import Utils as ut
+from importlib import reload
 
 # %% ----------------------------------------------------------------
 # LOAD PICKLED ML MATRICES
 
-X_train = pd.read_pickle('Data/PBMC 10k multiomic/processed_data/PCA/X_train.pkl')
-X_test = pd.read_pickle('Data/PBMC 10k multiomic/processed_data/PCA/X_test.pkl')
-y_train = np.load('Data/PBMC 10k multiomic/processed_data/PCA/y_train.npy', allow_pickle=True)
-y_test = np.load('Data/PBMC 10k multiomic/processed_data/PCA/y_test.npy', allow_pickle=True)
+Xpca_train = pd.read_pickle('Data/PBMC 10k multiomic/processed_data/X_Matrices/Xpca_train.pkl')
+Xpca_test = pd.read_pickle('Data/PBMC 10k multiomic/processed_data/X_Matrices/Xpca_test.pkl')
 
+y_train = np.load('Data/PBMC 10k multiomic/y_train.npy', allow_pickle=True)
+y_test = np.load('Data/PBMC 10k multiomic/y_test.npy', allow_pickle=True)
+y_train_mjr = np.load('Data/PBMC 10k multiomic/y_train_mjr.npy', allow_pickle=True)
+y_test_mjr = np.load('Data/PBMC 10k multiomic/y_test_mjr.npy', allow_pickle=True)
 # %% ----------------------------------------------------------------
 #ML Models
 
@@ -30,60 +33,38 @@ rf=RandomForestClassifier(random_state=42, class_weight='balanced')
 svm_model=svm.SVC(random_state=42, class_weight='balanced')
 
 # %% ----------------------------------------------------------------
-# REMOVE NANS
-
-# Create DataFrame from X_train and X_test
-train_data = pd.DataFrame(X_train)
-test_data = pd.DataFrame(X_test)
-
-# Add y_train and y_test as columns
-train_data['label'] = y_train
-test_data['label'] = y_test
-
-# Removing rows with NaN values
-train_data = train_data.dropna(subset=['label'])
-test_data = test_data.dropna(subset=['label'])
-
-# Separate X_train, y_train, X_test, and y_test from the updated DataFrame
-X_train = train_data.iloc[:, :-1]
-y_train = train_data['label'].to_numpy()
-
-X_test = test_data.iloc[:, :-1]
-y_test = test_data['label'].to_numpy()
-
-# %% ----------------------------------------------------------------
 # RNA ONLY FEATURE SET
 
-X_train_rna = X_train.filter(like='RNA')
-X_test_rna = X_test.filter(like='RNA')
+Xpca_train_rna = Xpca_train.filter(like='RNA')
+Xpca_test_rna = Xpca_test.filter(like='RNA')
 
-# %% ----------------------------------------------------------------
-# RANDOM FOREST RNA + ATAC
-
-model_cl, y_pred_test = ut.model_test_main(rf,X_train,y_train,
-                                           X_test,y_test, 
-                                           subset = False)
-ut.save_model(model_cl,'PCA\RF', y_pred_test, y_test)
 # %% ----------------------------------------------------------------
 # RANDOM FOREST RNA ONLY
 
-model_cl, y_pred_test = ut.model_test_main(rf,X_train_rna,y_train,
-                                           X_test_rna,y_test, 
+model_cl, y_pred_test = ut.model_test_main(rf,Xpca_train_rna,y_train,
+                                           Xpca_test_rna,y_test, 
                                            subset = False)
 ut.save_model(model_cl,'PCA\RF_RNA_only', y_pred_test, y_test)
-
 # %% ----------------------------------------------------------------
-# SVM RNA + ATAC
+# RANDOM FOREST RNA + ATAC
 
-model_cl, y_pred_test = ut.model_test_main(svm_model,X_train,y_train,
-                                           X_test,y_test, 
+model_cl, y_pred_test = ut.model_test_main(rf,Xpca_train,y_train,
+                                           Xpca_test,y_test, 
                                            subset = False)
 ut.save_model(model_cl,'PCA\RF', y_pred_test, y_test)
+
 # %% ----------------------------------------------------------------
 # SVM RNA ONLY
 
-model_cl, y_pred_test = ut.model_test_main(svm_model,X_train_rna,y_train,
-                                           X_test_rna,y_test, 
-                                           subset = False)
+model_cl, y_pred_test = ut.model_test_main(svm_model,Xpca_train_rna,y_train,
+                                           Xpca_test_rna,y_test, 
+                                           subset = True)
 ut.save_model(model_cl,'PCA\RF_RNA_only', y_pred_test, y_test)
+# %% ----------------------------------------------------------------
+# SVM RNA + ATAC
+
+model_cl, y_pred_test = ut.model_test_main(svm_model,Xpca_train,y_train,
+                                           Xpca_test,y_test, 
+                                           subset = False)
+ut.save_model(model_cl,'PCA\RF', y_pred_test, y_test)
 # %%

Train_test PBMC10k.py
@@ -9,8 +9,13 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import time
+from sklearn.cross_decomposition import CCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
-from Utils import train_test_split_mdata, pre_process
+import Utils as ut
+from importlib import reload
 # %% ----------------------------------------------------------------
 #LOAD DATA
 
@@ -20,59 +25,56 @@ mdata=mu.read_h5mu("Data/PBMC 10k multiomic/processed_data/QC-pbmc10k.h5mu")
 # %% ----------------------------------------------------------------
 # TRAIN-TEST SPLIT
 
-mdata_train, mdata_test = train_test_split_mdata(mdata)
+mdata_train, mdata_test = ut.train_test_split_mdata(mdata)
+
 # %% ----------------------------------------------------------------
 # PRE-PROCESSING RNA
 
-mdata_train.mod['rna'] = pre_process(mdata_train['rna'])
-mdata_test.mod['rna'] = pre_process(mdata_test['rna'])
-
+mdata_train.mod['rna'] = ut.pre_process_train(mdata_train['rna'])
+mdata_test.mod['rna'] = ut.pre_process_test(mdata_test['rna'], mdata_train['rna'])
 # %% ----------------------------------------------------------------
 # PRE-PROCESSING ATAC
 
-mdata_train.mod['atac'] = pre_process(mdata_train['atac'])
-mdata_test.mod['atac'] = pre_process(mdata_test['atac'])
-
-# %% ----------------------------------------------------------------
-# DIMENSIONALITY REDUCTION OF RNA
-
-for df, name in zip([mdata_train, mdata_test],['train', 'test']):
-    st=time.process_time()
-    sc.tl.pca(df.mod['rna'])
-    et=time.process_time()
-    print(f"{name} PCA (RNA) took {et-st} seconds")
-    sc.pl.pca_overview(mdata_train.mod['rna'])
-    sc.pl.pca(df.mod['rna'], color=['CD2', 'CD79A', 'KLF4', 'IRF8'])
-
-# %% ----------------------------------------------------------------
-# DIMENSIONALITY REDUCTION OF ATAC
-
-for df, name in zip([mdata_train, mdata_test],['train', 'test']):
-    st=time.process_time()
-    sc.tl.pca(df.mod['atac'])
-    et=time.process_time()
-    print(f"{name} PCA (ATAC) took {et-st} seconds")
-    sc.pl.pca_overview(df.mod['atac'])
-
-# %% ----------------------------------------------------------------
-# CHOOSING N_PCS
-
-sc.pl.pca_variance_ratio(mdata_train.mod['atac'], log=True) # 14 PCs
-print(np.cumsum(mdata_train.mod['atac'].uns['pca']['variance_ratio'])[:14]) # Explains 6.3% of variance
-sc.pl.pca_variance_ratio(mdata_train.mod['rna'], log=True) # 12 PCs
-print(np.cumsum(mdata_train.mod['rna'].uns['pca']['variance_ratio'])[:12]) # Explains 10.8% of variance
+mdata_train.mod['atac'] = ut.pre_process_train(mdata_train['atac'])
+mdata_test.mod['atac'] = ut.pre_process_test(mdata_test['atac'], mdata_train['atac'])
 
 # %% ----------------------------------------------------------------
 # SAVE PROCESSED DATA
 
-mdata_train.write_h5mu("Data/PBMC 10k multiomic/processed_data/PCA/unann_mdata_train.h5mu")
-mdata_test.write_h5mu("Data/PBMC 10k multiomic/processed_data/PCA/unann_mdata_test.h5mu")
+mdata_train.write_h5mu("Data/PBMC 10k multiomic/processed_data/pre-proc_mdata_train.h5mu")
+mdata_test.write_h5mu("Data/PBMC 10k multiomic/processed_data/pre-proc_mdata_test.h5mu")
 
 # %% ----------------------------------------------------------------
 # LOAD PROCESSED DATA
 
-mdata_train = mu.read_h5mu("Data/PBMC 10k multiomic/processed_data/PCA/unann_mdata_train.h5mu")
-mdata_test = mu.read_h5mu("Data/PBMC 10k multiomic/processed_data/PCA/unann_mdata_test.h5mu")
+mdata_train = mu.read_h5mu("Data/PBMC 10k multiomic/processed_data/pre-proc_mdata_train.h5mu")
+mdata_test = mu.read_h5mu("Data/PBMC 10k multiomic/processed_data/pre-proc_mdata_test.h5mu")
+
+# %% ----------------------------------------------------------------
+# DIMENSIONALITY REDUCTION - CCA
+
+sample_train = mu.pp.sample_obs(mdata_train,0.1)[:,0:30000]
+sample_test = mu.pp.sample_obs(mdata_test,0.1)[:,0:30000]
+
+sample_train, sample_test = ut.perform_cca(sample_train, sample_test, n_components=20)
+# %% ----------------------------------------------------------------
+# DIMENSIONALITY REDUCTION - PCA
+
+mdata_train, mdata_test, pca = ut.perform_pca(mdata_train, mdata_test)
+# RNA ELBOW = 10 PCS
+# ATAC ELBOW = 12 PCS
+
+# %% ----------------------------------------------------------------
+# SAVE PROCESSED DATA
+
+mdata_train.write_h5mu("Data/PBMC 10k multiomic/processed_data/Dim_Red/unann_mdata_train.h5mu")
+mdata_test.write_h5mu("Data/PBMC 10k multiomic/processed_data/Dim_Red/unann_mdata_test.h5mu")
+
+# %% ----------------------------------------------------------------
+# LOAD PROCESSED DATA
+
+mdata_train = mu.read_h5mu("Data/PBMC 10k multiomic/processed_data/Dim_Red/unann_mdata_train.h5mu")
+mdata_test = mu.read_h5mu("Data/PBMC 10k multiomic/processed_data/Dim_Red/unann_mdata_test.h5mu")
 # %% ----------------------------------------------------------------
 # ADDING ANNOTATIONS
 
@@ -96,25 +98,8 @@ for df, name in zip([mdata_train, mdata_test],['train', 'test']):
     # Count number of NAs in cell_type column
     print(f"{name} cell_type NAs: {df.obs['cell_type'].isna().sum()}")
 
- # %% ----------------------------------------------------------------
-# VISUALISE ANNOTATIONS RNA
-
-sc.set_figure_params(dpi=200) # set the dpi
-for df, name in zip([mdata_train.mod['rna'], mdata_test.mod['rna']],['train', 'test']):
-    sc.pp.neighbors(df, n_neighbors=10, n_pcs=12) 
-    sc.tl.umap(df, spread=1., min_dist=.5, random_state=11)
-    sc.pl.umap(df, color='cell_type', legend_loc='on data',legend_fontsize = 4, title=f'{name} UMAP (RNA)')
-
 # %% ----------------------------------------------------------------
-#VISUALISE ANNOTATIONS ATAC
-
-for df, name in zip([mdata_train.mod['atac'], mdata_test.mod['atac']],['train', 'test']):
-    sc.pp.neighbors(df, n_neighbors=10, n_pcs=14) 
-    sc.tl.umap(df, spread=1., min_dist=.5, random_state=11)
-    sc.pl.umap(df, color='cell_type', legend_loc='on data',legend_fontsize = 4, title=f'{name} UMAP (ATAC)')
-
-# %% ----------------------------------------------------------------
-# GENERATING ML MATRICES
+# GENERATING LABEL SETS
 
 # Generating labels for training set
 y_train = mdata_train.obs['cell_type'].values
@@ -122,23 +107,61 @@ y_train = mdata_train.obs['cell_type'].values
 # Generating labels for test set
 y_test = mdata_test.obs['cell_type'].values
 
-# Generating feature matrix for training set
-X_train = np.concatenate((mdata_train.mod['rna'].obsm['X_pca'][:,:12], mdata_train.mod['atac'].obsm['X_pca'][:,:14]), axis=1)
-# Convert to dataframe
-X_train = pd.DataFrame(X_train, columns=[f"RNA PC{i}" for i in range(1,13)] + [f"ATAC PC{i}" for i in range(1,15)])
-print(X_train.head())
+# CREATING DIFFERENT LABEL SETS
 
-# Generating feature matrix for test set
-X_test = np.concatenate((mdata_test.mod['rna'].obsm['X_pca'][:,:12], mdata_test.mod['atac'].obsm['X_pca'][:,:14]), axis=1)
-# Convert to dataframe
-X_test = pd.DataFrame(X_test, columns=[f"RNA PC{i}" for i in range(1,13)] + [f"ATAC PC{i}" for i in range(1,15)])
-print(X_test.head())
+# MAJOR CELL TYPE SET
+# Define your mapping dictionary
+removing_nans = {'Platelets':np.nan, 'Double negative T cell':np.nana}
+dict_map = {'CD8 Naive': 'CD8','CD8 Effector':'CD8', 'CD4 Memory': 'CD4', 'CD4 Naive': 'CD4',
+            'pre-B cell':'B cell progenitor', 'CD16+ Monocytes':'Monoblast-Derived', 
+            'CD14+ Monocytes':'Monoblast-Derived','Dendritic Cells':'Monoblast-Derived',
+            'pDC':'Monoblast-Derived'}
+# Create a vectorized function
+vfunc = np.vectorize(lambda x: dict_map.get(x, x))
+rem_nan_func = np.vectorize(lambda x: removing_nans.get(x, x))
+# Apply the vectorized function to your array
+y_train = rem_nan_func(y_train)
+y_test = rem_nan_func(y_test)
+y_train_mjr = vfunc(y_train)
+y_test_mjr = vfunc(y_test)
 
+# %% ----------------------------------------------------------------
+# GENERATING FEATURE MATRICES
+
+Xpca_train, Xpca_test, y_train, y_test = ut.generate_feature_matrix(mdata_train, mdata_test, 
+                                                   y_train, y_test, 'PCA', 
+                                                   n_components_rna=10, n_components_atac=12)   
+'''
+Xcca_train, Xcca_test = ut.generate_feature_matrix(mdata_train, mdata_test,
+                                                   y_train, y_test, 'CCA',
+                                                   n_components_rna=10, n_components_atac=12)
+'''
 # %% ----------------------------------------------------------------
 # SAVE ML MATRICES AS PICKLES
 
-X_train.to_pickle("Data/PBMC 10k multiomic/processed_data/PCA/X_train.pkl")
-X_test.to_pickle("Data/PBMC 10k multiomic/processed_data/PCA/X_test.pkl")
-np.save("Data/PBMC 10k multiomic/processed_data/PCA/y_train.npy", y_train)
-np.save("Data/PBMC 10k multiomic/processed_data/PCA/y_test.npy", y_test)
-# %%
+Xpca_train.to_pickle("Data/PBMC 10k multiomic/processed_data/X_Matrices/Xpca_train.pkl")
+Xpca_test.to_pickle("Data/PBMC 10k multiomic/processed_data/X_Matrices/Xpca_test.pkl")
+
+# %% ---------------------------------------------------------------- 
+# SAVE LABELS AS NUMPY ARRAYS
+np.save("Data/PBMC 10k multiomic/y_train.npy", y_train)
+np.save("Data/PBMC 10k multiomic/y_test.npy", y_test)
+np.save("Data/PBMC 10k multiomic/y_train_mjr.npy", y_train_mjr)
+np.save("Data/PBMC 10k multiomic/y_test_mjr.npy", y_test_mjr)
+
+# %% ----------------------------------------------------------------
+# VISUALISE GROUND TRUTH ANNOTATIONS RNA
+
+sc.set_figure_params(dpi=200) # set the dpi
+for df, name in zip([mdata_train.mod['rna'], mdata_test.mod['rna']],['train', 'test']):
+    sc.pp.neighbors(df, n_neighbors=10, n_pcs=10) 
+    sc.tl.umap(df, spread=1., min_dist=.5, random_state=11)
+    sc.pl.umap(df, color='cell_type', legend_loc='on data',legend_fontsize = 4, title=f'{name} UMAP (RNA)')
+
+# %% ----------------------------------------------------------------
+#VISUALISE GROUND TRUTH ANNOTATIONS ATAC
+
+for df, name in zip([mdata_train.mod['atac'], mdata_test.mod['atac']],['train', 'test']):
+    sc.pp.neighbors(df, n_neighbors=10, n_pcs=12) 
+    sc.tl.umap(df, spread=1., min_dist=.5, random_state=11)
+    sc.pl.umap(df, color='cell_type', legend_loc='on data',legend_fontsize = 4, title=f'{name} UMAP (ATAC)')

Utils.py
@@ -6,13 +6,18 @@ import h5py as h5
 import pandas as pd
 from muon import atac as ac
 import numpy as np
-from sklearn.model_selection import train_test_split
+import scipy
+from sklearn.cross_decomposition import CCA
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.model_selection import LearningCurveDisplay, train_test_split
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
+from sklearn.utils import resample
 from xgboost import XGBClassifier
 import time
 from sklearn.metrics import classification_report
@@ -46,10 +51,10 @@ def train_test_split_mdata(mdata_obj):
 
     return mdata_train, mdata_test
 
-def pre_process(adata):
+def pre_process_train(adata):
 
     '''
-    Pre-process MuData object for either RNA or ATAC modality
+    Pre-process MuData TRAIN object for either RNA or ATAC modality
     '''
 
     # Filter out low-frequency features
@@ -61,25 +66,191 @@ def pre_process(adata):
     # Normalizing peaks/genes
     print(f"Total peaks/genes in random cell: {adata.X[1,:].sum()}")
     sc.pp.normalize_total(adata, target_sum=1e4) # Normalize counts per cell
-    print(f"Total peaks/genes in random cell: {adata.X[1,:].sum()}")
+    print(f"Total peaks/genes in random cell (Should be 10000): {adata.X[1,:].sum()}") # Sanity check for normalization - should be 1e4
     sc.pp.log1p(adata) # Logarithmize + 1
-    print(f"Total peaks/genes in random cell: {adata.X[1,:].sum()}") # Sanity check for normalization - should be 1e4
+    print(f"Total peaks/genes in random cell: {adata.X[1,:].sum()}") 
 
     # Filtering features
     sc.pp.highly_variable_genes(adata) # Select highly variable genes
     sc.pl.highly_variable_genes(adata) # Plot highly variable genes
+    mu.pp.filter_var(adata, 'highly_variable', lambda x: x == True) # Filter out non-highly variable genes
     print(f"Number of highly variable peaks/genes: {np.sum(adata.var.highly_variable)}")
-    print(f"Number of peaks/genes before filtering: {adata.n_vars}")
-    #adata = adata[:, adata.var.highly_variable] # Filter out non-highly variable genes
-    print(f"Number of peaks/genes after filtering: {adata.n_vars}")
+    print(f"Number of peaks/genes after filtering: {adata.shape[1]}")
 
     # Scaling
     adata.raw = adata # Store unscaled counts in .raw attribute
     sc.pp.scale(adata) # Scale to unit variance and shift to zero mean
     print(f"Min Scaled peaks/genes value:{adata.X.min()}") 
+    print(f"Min unscaled peaks/genes value - Should not be negative: {adata.raw.X.min()}") # Sanity check for scaling - should not be negative
+
+    return adata
+
+def pre_process_test(adata, adata_train):
+
+    '''
+    Pre-process MuData TEST object for either RNA or ATAC modality using 
+    parameters from pre-processing of TRAIN object
+    '''
+
+    # Filtering features
+    mu.pp.filter_var(adata, adata_train.var.index) # Filter out non-highly variable genes
+    print(f"Number of peaks/genes after filtering: {adata.shape[1]}")
+
+    # Saving raw counts
+    adata.layers["counts"] = adata.X # Store unscaled counts in .layers["counts"] attribute
+
+    # Normalizing peaks/genes
+    print(f"Total peaks/genes in random cell: {adata.X[1,:].sum()}")
+    sc.pp.normalize_total(adata, target_sum=1e4) # Normalize counts per cell
+    print(f"Total peaks/genes in random cell (Should be 10000): {adata.X[1,:].sum()}") # Sanity check for normalization - should be 1e4
+    sc.pp.log1p(adata) # Logarithmize + 1
+    print(f"Total peaks/genes in random cell: {adata.X[1,:].sum()}") 
+
+    # Scaling
+    adata.raw = adata # Store unscaled counts in .raw attribute
+    adata.X = adata.X.toarray()
+    adata.X = (adata.X - adata_train.var['mean'].values[np.newaxis,:])/adata_train.var['std'].values[np.newaxis,:]
+    print(f"Min Scaled peaks/genes value:{adata.X.min()}") 
     print(f"Min unscaled peaks/genes value: {adata.raw.X.min()}") # Sanity check for scaling - should not be negative
 
     return adata
+
+def perform_pca(mdata_train, mdata_test, components=20, random_state=42):
+    '''
+    Perform PCA on RNA and ATAC modalities of given mdata_train and mdata_test
+    '''
+    pca ={}
+    for mod in ['rna', 'atac']:
+        st=time.process_time()
+        pca[mod] = PCA(n_components=components, random_state=random_state).fit(mdata_train.mod[mod].X)
+
+        # Transform count matrix using pca and store in mdata object
+        mdata_train.mod[mod].obsm['X_pca'] = pca[mod].transform(mdata_train.mod[mod].X)
+        mdata_test.mod[mod].obsm['X_pca'] = pca[mod].transform(mdata_test.mod[mod].X)
+        et=time.process_time()
+        mdata_train.mod[mod].varm['PCs'] = np.transpose(pca[mod].components_)
+        print(f"PCA {mod} took {et-st} seconds")
+
+        # Scree plot
+        PC_values = np.arange(pca[mod].n_components_) + 1
+        plt.plot(PC_values, pca[mod].explained_variance_ratio_, 'ro-', linewidth=2)
+        plt.title('Scree Plot')
+        plt.xlabel('Principal Component')
+        plt.ylabel('Proportion of Variance Explained')
+        plt.show()
+    
+        # Ask user input for desired number of PCs and store it
+        num_pcs = int(input(f"Enter the number of PCs you want to keep for {mod}: "))
+        print(f"PCA {mod} with {num_pcs} PCs explains {np.cumsum(pca[mod].explained_variance_ratio_[0:num_pcs])[-1]*100}% of variance")
+
+    return mdata_train, mdata_test, pca
+
+def perform_cca(mdata_train, mdata_test, n_components=20):
+    '''
+    Performs CCA on the data and adds the CCA components to the mdata object.
+    Also generates scree plot of correlation between each CCA component.
+    '''
+    # Perform CCA
+    cca = CCA(n_components=n_components, scale=False)
+    st=time.process_time()
+    cca.fit(mdata_train.mod['rna'].X, mdata_train.mod['atac'].X)
+    rna_train, atac_train = cca.transform(mdata_train.mod['rna'].X, mdata_train.mod['atac'].X)
+    rna_test, atac_test = cca.transform(mdata_test.mod['rna'].X, mdata_test.mod['atac'].X)
+    et=time.process_time()
+    print('CCA took {} seconds'.format(et-st))
+    
+    # Add CCA components to mdata
+    mdata_train.mod['rna'].obsm['cca'] = rna_train
+    mdata_train.mod['atac'].obsm['cca'] = atac_train
+    mdata_test.mod['rna'].obsm['cca'] = rna_test
+    mdata_test.mod['atac'].obsm['cca'] = atac_test
+
+    # Scree plot
+    # sklearn CCA doesn't directly provide the canonical correlations,
+    # so we compute them as follows:
+    correlations = [np.corrcoef(rna_train[:,i], atac_train[:,i])[0,1] for i in range(rna_train.shape[1])]
+
+    # Plot the square of the canonical correlations
+    eigenvalues = np.square(correlations)
+    plt.plot(range(1, len(eigenvalues) + 1), eigenvalues, 'ro-', linewidth=2)
+    plt.title('Scree Plot')
+    plt.xlabel('Canonical Variate')
+    plt.ylabel('Squared Canonical Correlation')
+    plt.show()
+
+    return mdata_train, mdata_test
+
+def generate_feature_matrix(mdata_train, mdata_test, y_train, y_test, embedding, n_components_rna, n_components_atac):
+    if embedding == 'PCA':
+        obsm = 'X_pca'
+    elif embedding == 'CCA':
+        obsm = 'cca'
+    # Generating feature matrix for training set
+    X_train = np.concatenate((mdata_train.mod['rna'].obsm[obsm][:,:n_components_rna], mdata_train.mod['atac'].obsm[obsm][:,:n_components_atac]), axis=1)
+    # Convert to dataframe
+    X_train = pd.DataFrame(X_train, columns=[f"RNA Comp{i}" for i in range(1,n_components_rna+1)] + [f"ATAC Comp{i}" for i in range(1,n_components_atac+1)])
+    print(X_train.head())
+
+    # Generating feature matrix for test set
+    X_test = np.concatenate((mdata_test.mod['rna'].obsm[obsm][:,:n_components_rna], mdata_test.mod['atac'].obsm[obsm][:,:n_components_atac]), axis=1)
+    # Convert to dataframe
+    X_test = pd.DataFrame(X_test, columns=[f"RNA Comp{i}" for i in range(1,n_components_rna+1)] + [f"ATAC Comp{i}" for i in range(1,n_components_atac+1)])
+    print(X_test.head())
+
+    # Standardization
+    sclr = StandardScaler().fit(X_train)
+    X_train = sclr.transform(X_train)
+    X_test = sclr.transform(X_test)
+
+    # Convert back to dataframe format with column names
+    X_train = pd.DataFrame(X_train, columns=[f"RNA PC{i}" for i in range(1,n_components_rna+1)] + [f"ATAC PC{i}" for i in range(1,n_components_atac+1)])
+    X_test = pd.DataFrame(X_test, columns=[f"RNA PC{i}" for i in range(1,n_components_rna+1)] + [f"ATAC PC{i}" for i in range(1,n_components_atac+1)])
+
+    # Create DataFrame from X_train and X_test
+    train_data = pd.DataFrame(X_train)
+    test_data = pd.DataFrame(X_test)
+
+    # Add y_train and y_test as columns
+    train_data['label'] = y_train
+    test_data['label'] = y_test
+
+    # Removing rows with NaN values
+    train_data = train_data.dropna(subset=['label'])
+    test_data = test_data.dropna(subset=['label'])
+
+    # Separate X_train, y_train, X_test, and y_test from the updated DataFrame
+    X_train = train_data.iloc[:, :-1]
+    y_train = train_data['label'].to_numpy()
+
+    X_test = test_data.iloc[:, :-1]
+    y_test = test_data['label'].to_numpy()
+
+    print(X_train.head())
+    print(X_test.head())
+    print(set(y_train))
+    print(set(y_test))
+    return X_train, X_test, y_train, y_test
+
+def bootstrap_confidence_interval(model, X, y, n_bootstrap=1000):
+    """
+    Function to estimate a 95% confidence interval for a model's F1 score using bootstrapping.
+    """
+    f1_scores = []
+    for _ in range(n_bootstrap):
+        X_resample, y_resample = resample(X, y, n_samples=len(X) // 2)
+        y_pred = model.predict(X_resample)
+        f1_scores.append(f1_score(y_resample, y_pred, average='macro'))
+    lower = np.percentile(f1_scores, 2.5)
+    upper = np.percentile(f1_scores, 97.5)
+    mean = np.mean(f1_scores)
+    median = np.median(f1_scores)
+    # Plot histogram of F1 scores
+    plt.hist(f1_scores)
+    plt.title("Histogram of F1 scores")
+    plt.xlabel("F1 score")
+    plt.ylabel("Frequency")
+    plt.show()
+    return lower, upper, mean, median
 
 def model_test_main(model,x_train,y_train,x_test,y_test, subset, table_one_only=False):
     '''
@@ -100,7 +271,7 @@ def model_test_main(model,x_train,y_train,x_test,y_test, subset, table_one_only=
         y_test = encoder.transform(y_test)
     elif  isinstance(model, RandomForestClassifier):
         param_grid={'n_estimators'   : np.array([20, 40, 60]),
-                    'min_samples_leaf'   : np.array([1, 2, 5]),
+                    'min_samples_leaf'   : np.array([5, 10]),
                     'max_features' : np.array(['sqrt']),
                     'max_depth':np.array([2**2, 2**3, 2**4]),
                     'min_samples_split': np.array([3, 5, 10])}
@@ -122,6 +293,8 @@ def model_test_main(model,x_train,y_train,x_test,y_test, subset, table_one_only=
         model =RandomForestClassifier(**optimal_params, random_state=42, class_weight='balanced')    
     elif isinstance(model, svm.SVC):
         model=svm.SVC(**optimal_params, random_state=42, probability=True, class_weight='balanced')
+    LearningCurveDisplay.from_estimator(model, x_train, y_train, random_state=42)
+    plt.show()
     #Fitting model with optimised parameters to training data
     model.fit(x_train,y_train)
 
@@ -145,6 +318,14 @@ def model_test_main(model,x_train,y_train,x_test,y_test, subset, table_one_only=
         plt.show(cmatrix)
     time_taken=(time.process_time() - start_time)
     print(f'CPU time for training and testing: {time_taken} seconds or {time_taken/60} mins or {time_taken/(60*60)} hrs')
+
+    # Estimate confidence interval for F1 score
+    start_time = time.process_time()
+    lower, upper, mean, median = bootstrap_confidence_interval(model, x_test, y_test)
+    time_taken=(time.process_time() - start_time)
+    print(f"95% confidence interval for F1 score: ({lower:.3f}, {upper:.3f}, mean: {mean:.3f}, median: {median:.3f})")
+    print(f'CPU time for boostrap: {time_taken} seconds or {time_taken/60} mins or {time_taken/(60*60)} hrs')
+
     return(model, y_pred_test)
 
 def save_model(model_cl, location, y_pred_test, y_test):